7087c15429275fd9028f5040ffdf114c9d53b9dc
root <root@LAPTOP-9E7P7PNQ.localdomain>
2020-08-18 15:29:18 +0530

    Updates

---
+++ precache-manifest.d152962279140d6fb87293ea749dfa77.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18da03b5b776ae0618d912b6675bdcf3",
+    "url": "/notes-app-react/index.html"
+  },
+  {
+    "revision": "e9a5192131d0bf7ac9d0",
+    "url": "/notes-app-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b086e4ff6e04020623e8",
+    "url": "/notes-app-react/static/css/main.5fa6ee1b.chunk.css"
+  },
+  {
+    "revision": "e9a5192131d0bf7ac9d0",
+    "url": "/notes-app-react/static/js/2.56f46137.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/notes-app-react/static/js/2.56f46137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b086e4ff6e04020623e8",
+    "url": "/notes-app-react/static/js/main.61287324.chunk.js"
+  },
+  {
+    "revision": "7cb7a5f4ecd93a39c08b",
+    "url": "/notes-app-react/static/js/runtime-main.b042465e.js"
+  },
+  {
+    "revision": "e6756c34c0faa510f2ec49ff17d60988",
+    "url": "/notes-app-react/static/media/archive.e6756c34.png"
+  },
+  {
+    "revision": "c93b5ac1352c79a3ac4a0c2bda04dc80",
+    "url": "/notes-app-react/static/media/logo.c93b5ac1.png"
+  },
+  {
+    "revision": "0f1c59dc9e5ac99103a5dfad852f91b1",
+    "url": "/notes-app-react/static/media/notes.0f1c59dc.png"
+  },
+  {
+    "revision": "44cc57c0b0dd53b0d9948020533c3cdc",
+    "url": "/notes-app-react/static/media/reminder.44cc57c0.png"
+  },
+  {
+    "revision": "2c3e6019a6a08b739bb607a4fc17ed38",
+    "url": "/notes-app-react/static/media/trash.2c3e6019.png"
+  }
+]);
